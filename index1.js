15c483d4b630d2a7c1e88d13ce66a58243bab568
Bibash9543 <140886542+Bibash9543@users.noreply.github.com>
2024-01-02 18:06:13 +0545

    bmi calculator advance

--- index1.js
+++ index1.js
@@ -12,3 +12,22 @@ if(n>30 && n<=70){
 if(n<=30){
     alert("Cant say anything");
 }
+//bmi calculator
+
+function bmiCalculator (weight, height) {
+    var bmi = weight/(height*height);
+    return Math.round(bmi);
+}
+
+var bmi = bmiCalculator(63,1.8);
+alert("your bmi is " + bmi);
+if(bmi<18.5){
+    alert("Your BMI is" + bmi + " " + "so you are underweight.")
+}
+if(bmi>=18.5 && bmi<=24.9){
+    alert("Your BMI is" + bmi + " " + "so you are normal weight")
+}
+if(bmi>24.5){
+    alert("Your BMI is" + bmi + " " + "so you are overweight.")
+}
+
