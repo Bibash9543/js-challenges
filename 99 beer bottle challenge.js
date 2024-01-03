4f0e5a93f971bd48ba5f2602bff0687ab21a42fc
Bibash9543 <140886542+Bibash9543@users.noreply.github.com>
2024-01-03 18:32:35 +0545

    bottle_challenge

---
+++ 99 beer bottle challenge.js
@@ -0,0 +1,11 @@
+var count = 99;
+
+    function bear(){
+        while(count>=1){
+            console.log(count +"bottles of beer on the wall, " + count + "bottles of beer.");
+            count--;
+            console.log("Take one down and pass it around, " +count+" bottles of beer on the wall.")
+        }
+        
+        
+    }
