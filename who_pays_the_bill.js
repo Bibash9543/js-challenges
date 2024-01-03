6f56ae5f3ecb37a51f8fd356585710bca38d84b2
Bibash9543 <140886542+Bibash9543@users.noreply.github.com>
2024-01-03 17:51:30 +0545

    random_person_generator

---
+++ who_pays_the_bill.js
@@ -0,0 +1,15 @@
+function whosPaying(names) {
+    
+    /******Don't change the code above*******/
+        
+        //Write your code here
+        var input1 = names.length;
+        var random = Math.random();
+        random = Math.floor(random*input1);
+        var randomPerson = names[random];
+        return randomPerson + " is going to buy lunch today!"
+        
+    
+    
+    /******Don't change the code below*******/    
+    }
